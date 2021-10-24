9b2c3a48ea04ca9e97c325f51a360abf3318d331
unknown <rochefeuille.ophelie@gmail.com>
2021-10-24 16:28:00 +0200

    chemin header principal ok

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -25,4 +25,37 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 @import "../assets/variables_scss/variables.scss";
+header {
+  position: sticky;
+  top: 0;
+  z-index: 2;
+  background-color: #ffffff;
+  padding-top: 20px;
+}
+
+.header-main {
+  display: flex;
+  justify-content: space-between;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.buttons-main {
+  display: flex;
+  align-items: center;
+}
+
+.logo-lovood {
+  width: 70px;
+}
+.button-header {
+  margin-left: 50px;
+  margin-right: 10px;
+  text-decoration: none;
+  border: none;
+  background: none;
+  font-family: "quicksand-bold";
+  font-size: 28px;
+  color: #4e4e4e;
+}
 </style>
